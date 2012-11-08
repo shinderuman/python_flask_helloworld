15bfa2d035674259335d10c10291a0e1f927b254
shinderuman <shinderuman@gmail.com>
2012-11-09 02:00:34 +0900

    add mysql connect

--- app.py
+++ app.py
@@ -1,13 +1,27 @@
 from flask import Flask
+import MySQLdb
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+  return 'Hello World!'
 
 @app.route('/world')
-def hello_world():
-    return 'World!'
+def world():
+  return 'World!'
+
+@app.route('/db')
+def db():
+  connect = MySQLdb.connect(db='foo', host='127.0.0.1', port=3306, user='root', passwd='hoge')
+  cur = connect.cursor()
+  cur.execute('select text from bar')
+  rows = cur.fetchall()
+  str = ''
+  for row in rows:
+    str += row[0]
+  cur.close()
+  connect.close
+  return str
 
 if __name__ == '__main__':
-    app.run()
+  app.run()
